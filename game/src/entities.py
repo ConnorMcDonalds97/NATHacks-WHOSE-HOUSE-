3eb35238f05e1a83a744c192f5400fc81680d4c2
Ben B <bbui@ualberta.ca>
2025-11-08 09:30:26 -0700

    updated tile

--- game/src/entities.py
+++ game/src/entities.py
@@ -4,26 +4,51 @@ class Tile():
     def __init__(self, width, height):
         self.pos = pygame.Vector2(0,0)
         self.vel = pygame.Vector2(0,0)
-        self.width = 0
-        self.height = 0
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        return f"Pos: {self.pos}\nVel: {self.vel}\nWidth: {self.width}\nHeight: {self.height}"
 
     def calculateCentreScreenX(self, screenWidth):
         return (screenWidth)/2 - (self.width/2)
 
-    def setPosition(self, x, y):
-        self.pos = pygame.Vector2(x,y)
-
-    def setVelocity(self, x, y):
+    # Setters
+    
+    def setVelocity(self, x: float, y: float):
         self.vel = pygame.Vector2(x,y)
 
+    def setPosition(self, x: float, y: float):
+        self.pos = pygame.Vector2(x,y)
+
+    def setDimensions(self, width: int, height: int):
+        self.width = width
+        self.height = height
     '''
     Updates position based on velocity * deltaTime
     '''
     def updatePos(self, deltaTime):
         self.pox += self.vel * deltaTime
 
+    # Getters
+
     def getVelocity(self):
         return self.vel
     
     def getPosition(self):
         return self.pos
+    
+    def getDimensions(self):
+        return (self.width, self.height)
+    
+def main():
+    print("testing tile")
+    tile = Tile(1000,0)
+    print(tile)
+    tile.setPosition(100,100)
+    print(tile.getPosition())
+    tile.setVelocity(5,0)
+    print()
+
+if __name__ == "__main__":
+    main()
