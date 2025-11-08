430a0ed80fb4341ecd7fb4f41dddaad2affbc63e
AbdouMurad <abdou@Abdou>
2025-11-08 15:00:54 -0700

    scaled tiles and used pygame.clock

--- game/src/main.py
+++ game/src/main.py
@@ -16,15 +16,15 @@ def main():
     game = Game(pygame.display.set_mode((const.SCREEN_WIDTH, const.SCREEN_HEIGHT)))
     game.draw()
 
+    clock = pygame.time.Clock()
+
     prevTime = time.time()
     dt = 0
     timer = 0
     while running:
         game.draw()
 
-        now = time.time()
-        dt = now - prevTime
-        prevTime = now
+        dt = clock.tick(60)/1000.0
 
         timer += dt
 
