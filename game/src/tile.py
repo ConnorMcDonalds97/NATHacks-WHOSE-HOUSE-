fded4ae4bd443ea3c542ba051f0b9ac05cde5511
Ben B <bbui@ualberta.ca>
2025-11-08 08:56:05 -0700

    tile class outline

--- game/src/tile.py
+++ game/src/tile.py
@@ -6,10 +6,20 @@ class Tile():
         self.posY = 0
         self.width = 0
         self.height = 0
+        self.vel = [0,0]
 
     def calculateCentreScreenX(self, screenWidth):
         return (screenWidth)/2 - (self.width/2)
-    
-    def updatePos(self, x, y):
-        self.posX = x
-        self.posY = y
+
+    def teleport(self, x, y):
+        pass
+
+    def setVelocity(self, x, y):
+        pass
+
+    def getVelocity(self):
+        pass
+
+    def updatePos(self, delta_time):
+        self.posX = self.vel[0] * delta_time
+        self.posY = self.vel[1] * delta_time
