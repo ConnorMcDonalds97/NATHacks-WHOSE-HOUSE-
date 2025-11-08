eebce7dfb4c3f7662b65c679b3e7f209ad1b4b1a
Ben B <bbui@ualberta.ca>
2025-11-08 08:49:43 -0700

    more game stuff

--- game/src/main.py
+++ game/src/main.py
@@ -1,10 +1,11 @@
 from pickle import TRUE
 import pygame
+import tile
 
 pygame.init()
 
-SCREEN_WIDTH = 1920
-SCREEN_HEIGHT = 1080
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
 
 COLOURS = {
     "WHITE": (255,255,255),
@@ -13,6 +14,12 @@ COLOURS = {
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+# noteSensor = tile.Sensor(1000,10)
+
+
+sensorPosX = (SCREEN_WIDTH/2) - (1000/2)
+noteSensor = pygame.Rect((sensorPosX,600, 1000, 10))
+
 def main():
     clock = pygame.time.Clock()
     running = True
@@ -20,3 +27,15 @@ def main():
     while running:
         # clock.tick returns milliseconds
         deltaTime = clock.tick(60) / 1000.0
+
+        pygame.draw.rect(screen, COLOURS["WHITE"], noteSensor)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.update()
+
+    pygame.quit()
+
+main()
