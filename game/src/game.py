e52c504107485214ce601915723be2c76dcc8b6e
Ben B <bbui@ualberta.ca>
2025-11-08 10:38:50 -0700

    updated tiles init and added draw functio

--- game/src/game.py
+++ game/src/game.py
@@ -1,15 +1,16 @@
 import pygame
 import const
 import entities
+
 class Game:
     def __init__(self, surface):
         self.bgRect = (0,0, const.SCREEN_WIDTH, const.SCREEN_HEIGHT) #x,y,width,height
         self.surface = surface
-
         self.sensor = entities.Tile(const.SCREEN_WIDTH, 5, 0, 920, 'white')
+        self.tiles = initTiles()
+        self.score = 0
 
-        self.tiles = []
-        self.initTiles()
+
     def showBg(self):
         pygame.draw.rect(self.surface, 'black', self.bgRect)
         
@@ -23,3 +24,11 @@ class Game:
 
     def initTiles(self):
         self.tiles = [entities.Tile(const.TILE_WIDTH,const.TILE_HEIGHT, const.SCREEN_WIDTH/2, 0, "white")]
+
+    def draw(self):
+        self.showBg()
+        self.showTiles()
+        self.showSensor()
+
+def initTiles():
+    return [entities.Tile(const.TILE_WIDTH,const.TILE_HEIGHT, const.SCREEN_WIDTH/2, 0, "white")]
--- game/src/main.py
+++ game/src/main.py
@@ -16,10 +16,6 @@ def main():
     while running:
         # clock.tick returns milliseconds
         deltaTime = clock.tick(60) / 1000.0
-        
-        game.showBg()
-        game.showSensor()
-        game.showTiles()
 
         for tile in game.tiles:
             # print(tile)
@@ -33,8 +29,8 @@ def main():
                     game.sensor.setColour("red")
             elif event.type == pygame.KEYUP:
                     game.sensor.setColour("white")
-        game.showSensor()
-                
+                    
+        game.draw()
 
         pygame.display.update()
 
