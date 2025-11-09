cc691d02da4de6dd0a3273392745362deeb27195
AbdouMurad <abdou@Abdou>
2025-11-08 17:28:47 -0700

    optimized tile movement

--- game/src/main.py
+++ game/src/main.py
@@ -26,17 +26,23 @@ def main():
         dt = clock.tick(60)/1000.0
 
         timer += dt
-
-        for tile in game.tiles1:
-            tile.updatePos(dt)
-        for tile in game.tiles2:
-            tile.updatePos(dt)
-        for tile in game.tiles3:
-            tile.updatePos(dt)
-        for tile in game.tiles4:
-            tile.updatePos(dt)
-       
-
+        
+        game.updateTiles(dt)
+        
+        #check if tile is below sensor -> if it is then increment front
+        f1 = game.tiles1
+        if (f1.front <  f1.len) and f1.data[f1.front].pos[1] > const.SCREEN_HEIGHT:
+            f1.front += 1
+        f2 = game.tiles2
+        if (f2.front <  f2.len) and f2.data[f2.front].pos[1] > const.SCREEN_HEIGHT:
+            f2.front += 1
+        f3 = game.tiles3
+        if (f3.front <  f3.len) and f3.data[f3.front].pos[1] > const.SCREEN_HEIGHT:
+            f3.front += 1
+        f4 = game.tiles4
+        if (f4.front <  f4.len) and f4.data[f4.front].pos[1] > const.SCREEN_HEIGHT:
+            f4.front += 1
+            
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
