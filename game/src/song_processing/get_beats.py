33d0f8111dcc56e5d560f7c733eb775bff89712d
oliveoil <ocai@ualberta.ca>
2025-11-08 17:22:42 -0700

    simplified notes yay

--- game/src/song_processing/get_beats.py
+++ game/src/song_processing/get_beats.py
@@ -1,10 +1,10 @@
 '''
 TO BE INVOKED IN THE PYGAME SCRIPT!
 '''
-from .assign_data_to_keys import *
-from  .get_beats_json import *
+from assign_data_to_keys import *
+from  get_beats_json import *
 
-def return_keys_assignments_and_populate_json(song_title, beat_type, num_sensors=4, instrument=-1):
+def return_keys_assignments_and_populate_json(song_title, beat_type, num_sensors=4, instrument=-1, min_note_duration=0.2, max_simultaneous_notes=2, time_between_notes=2):
     '''
     Example:
     song_title = 'Never Gonna Give You Up' (or any string to be queried from the songs json)
@@ -12,5 +12,11 @@ def return_keys_assignments_and_populate_json(song_title, beat_type, num_sensors
     num_sensors = 4 or the number of keys to be displayed on the screen
     instrument = -1 for detect the melody instrument, or specify an int [0,127]
     '''
-    return_beat_timestamps(song_title, beat_type, num_sensors, instrument)
+    return_beat_timestamps(song_title, beat_type, num_sensors, instrument, min_note_duration, max_simultaneous_notes, time_between_notes)
     return get_pitch_categories(beat_type, num_sensors)
+
+if __name__ == '__main__':
+    beats = return_keys_assignments_and_populate_json("Bohemian Rhapsody", beat_type=1, num_sensors=4, instrument=-1)
+    for b in beats:
+        print(b)
+        print("\n")
